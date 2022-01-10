c4c01d11ef0fa4fc43210920cbbea5bfe145f5b3
CyrielleAlbert <Cyriellealbert@yahoo.fr>
2022-01-10 17:14:32 +0100

    train_test_split

---
+++ utils.py
@@ -0,0 +1,13 @@
+﻿from os import listdir
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+PATH_DATASET = "./PokemonData/"
+
+def train_test_split(path,img_size,seed,validation_split):
+    labels = listdir(path)
+    dataset_train = image_dataset_from_directory(path,label_mode = 'int',labels="inferred",class_names=labels, validation_split=validation_split,image_size=img_size,
+    subset="training",seed=seed)
+    dataset_valid = image_dataset_from_directory(path,label_mode = 'int',labels="inferred",class_names=labels, validation_split=validation_split,image_size=img_size,
+    subset="validation",seed=seed)
+    
+    return dataset_train,dataset_valid
