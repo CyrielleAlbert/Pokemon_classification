1dc872d44b02a4dc5719a0ce7829dcd1306190a2
CyrielleAlbert <Cyriellealbert@yahoo.fr>
2022-01-11 21:07:13 +0100

    remove DS_store for mac

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 PATH_DATASET = "./PokemonData/"
 
 def train_test_split(path, img_size,seed,validation_split):
-    labels = listdir(path)
+    labels = [ f for f in listdir(path) if not f.startswith(".DS_Store")]
     dataset_train = image_dataset_from_directory(path, labels="inferred", label_mode = 'int',class_names=labels, validation_split=validation_split,image_size=img_size,
     subset="training",seed=seed)
     dataset_valid = image_dataset_from_directory(path, labels="inferred", label_mode = 'int', class_names=labels, validation_split=validation_split,image_size=img_size,
