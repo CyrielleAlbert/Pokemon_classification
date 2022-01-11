e5926dadd5b53d26613c5d34fec29e8c1b69d0e3
CyrielleAlbert <Cyriellealbert@yahoo.fr>
2022-01-11 20:47:51 +0100

    a

--- CNN_classification.py
+++ CNN_classification.py
@@ -6,11 +6,13 @@ Created on Mon Jan 10 13:56:57 2022
 @author: sarramargi
 """
 
-import numpy as np # linear algebra
+import numpy as np
+from sklearn.utils import validation # linear algebra
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Dropout, Dense, Flatten, InputLayer, concatenate
 from tensorflow.keras.layers import Conv2DTranspose
 from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
 
 import cv2 as cv
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
@@ -31,44 +33,50 @@ from sklearn.neighbors import KNeighborsClassifier
 import os
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from utils import train_test_split
+from utils import train_test_split2
+from sklearn.model_selection import train_test_split
 
 # Test prepreocess data
 PATH = './PokemonData'
 classes = os.listdir(PATH)
 
-c1_path=os.path.join(PATH, classes[1]) # different folders of Pokemon, considered as outputs
-c1_data_path=[os.path.join(c1_path, img) for img in os.listdir(c1_path)]
-len(c1_data_path)
+# c1_path=os.path.join(PATH, classes[1]) # different folders of Pokemon, considered as outputs
+# c1_data_path=[os.path.join(c1_path, img) for img in os.listdir(c1_path)]
+# len(c1_data_path)
 
 # Preprocessing 
-image_size = (256,256)
+image_size = (150,150)
 seed=5
 validation_split=0.2
-dataset_train,dataset_test = train_test_split(PATH,image_size,seed,validation_split)
+X,y = train_test_split2(PATH,image_size,seed,validation_split)
 
-# Normailisation
-IDG = ImageDataGenerator(rescale = 1./255 )
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
-train_data = IDG.flow_from_directory(PATH,target_size=(256,256),batch_size=32) 
+# # Normailisation
+# IDG = ImageDataGenerator(rescale = 1./255 )
 
-img_shape=(256,256,3) # 3 car RGB
+# train_data = IDG.flow_from_directory(PATH,target_size=(256,256),batch_size=32) 
+
+img_shape=(150,150,3) # 3 car RGB
 
 # Model | CNN Classique
 model_classic_CNN = keras.Sequential(name='Classic_CNN')
-model_classic_CNN.add(keras.layers.Conv2D(128,3,input_shape=(img_shape),activation='relu'))
-model_classic_CNN.add(keras.layers.MaxPool2D())
-model_classic_CNN.add(keras.layers.Conv2D(128,3,activation='relu'))
-model_classic_CNN.add(keras.layers.MaxPool2D())
-model_classic_CNN.add(keras.layers.Conv2D(128,3,strides=(2,2),activation='relu'))
-model_classic_CNN.add(keras.layers.MaxPool2D())
-model_classic_CNN.add(keras.layers.BatchNormalization())
-model_classic_CNN.add(keras.layers.Conv2D(64,3,strides=(2,2),activation='relu'))
-model_classic_CNN.add(keras.layers.MaxPool2D())
-model_classic_CNN.add(keras.layers.Flatten())
+model_classic_CNN.add(Rescaling(1./255,input_shape=img_shape))
+model_classic_CNN.add(keras.layers.Conv2D(120,kernel_size=(5,5),input_shape=(img_shape),activation='sigmoid'))
+model_classic_CNN.add(keras.layers.MaxPool2D(pool_size=(2,2),strides=(2,2)))
+# model_classic_CNN.add(keras.layers.Conv2D(128,3,activation='relu'))
+# model_classic_CNN.add(keras.layers.MaxPool2D())
+# model_classic_CNN.add(keras.layers.Conv2D(128,3,strides=(2,2),activation='relu'))
+# model_classic_CNN.add(keras.layers.MaxPool2D())
+# model_classic_CNN.add(keras.layers.BatchNormalization())
+# model_classic_CNN.add(keras.layers.Conv2D(64,3,strides=(2,2),activation='relu'))
+# model_classic_CNN.add(keras.layers.MaxPool2D())
 model_classic_CNN.add(keras.layers.Dropout(0.2))
-model_classic_CNN.add(keras.layers.Dense(1024,activation='relu'))
-model_classic_CNN.add(keras.layers.Dense(512,activation='relu'))
+model_classic_CNN.add(keras.layers.Flatten())
+model_classic_CNN.add(keras.layers.Dense(120,activation='sigmoid',kernel_initializer='uniform'))
+#model_classic_CNN.add(keras.layers.Dense(10,activation='sigmoid',kernel_initializer='uniform'))
 model_classic_CNN.add(keras.layers.Dense(len(classes),activation='softmax'))
 
 model_classic_CNN.summary()
@@ -78,7 +86,7 @@ model_classic_CNN.compile(optimizer='adam',
              metrics=['accuracy']
              )
 
-hist = model_classic_CNN.fit_generator(train_data,epochs=2) #Changer nb epochs
+hist = model_classic_CNN.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=100,batch_size=32) #Changer nb epochs
 
 plt.style.use('fivethirtyeight')
 plt.figure(figsize=(14,14))
@@ -90,15 +98,15 @@ plt.show()
 
 plt.figure(figsize=(20,20))
 #for _ in range(3):
-sam_x,sam_y = next(train_data) 
-pred_ = model_classic_CNN.predict(sam_x)
-for i in range(len(sam_x)):
-    pred,y = pred_[i].argmax(), sam_y[i].argmax()
-    plt.subplot(4,4,i+1)
-    plt.imshow(sam_x[i])
-    title_ = 'Predict:' + str(classes[pred])+ ';   Label:' + str(classes[y])
-    plt.title(title_,size=11)
-plt.show()
+# sam_x,sam_y = next(train_data) 
+# pred_ = model_classic_CNN.predict(sam_x)
+# for i in range(len(sam_x)):
+#     pred,y = pred_[i].argmax(), sam_y[i].argmax()
+#     plt.subplot(4,4,i+1)
+#     plt.imshow(sam_x[i])
+#     title_ = 'Predict:' + str(classes[pred])+ ';   Label:' + str(classes[y])
+#     plt.title(title_,size=11)
+# plt.show()
 
 
 #GridSearchCV 
