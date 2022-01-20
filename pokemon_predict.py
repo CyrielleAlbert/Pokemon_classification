a7b6aa3e8d5e621ebb52bd52a529eea2d6ae0d57
Sarra Margi <91186514+sarramargi@users.noreply.github.com>
2022-01-20 17:43:15 +0100

    Update predict

--- pokemon_predict.py
+++ pokemon_predict.py
@@ -18,14 +18,15 @@ from tensorflow.keras.models import load_model
 import matplotlib.image as mpimg
 import numpy as np
 
-# Pre-processing step
-def preprocess_img():
+#%% Pre-processing step
+def preprocess_img(img_init):
+    img_reshaped = img_init.reshape
     return img_prepro
 
+#%% Load the model
 # File path
 filepath = './unetFit.h5'
 
-# Load the model
 model = load_model(filepath)
 
 # Generate predictions for samples
@@ -36,5 +37,42 @@ predictions = model.predict(image_test_reshaped)
 print(predictions)
 
 # Check if predictions contains 1 which corresponds to the mask 
+one_value = np.argwhere(predictions>0)
+print(one_value)
+
+#%%
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 14 10:52:14 2022
+
+@author: sarramargi
+"""
+
+from tensorflow.keras.models import load_model
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy import asarray
+from preprocess_img import preprocess_img
+from PIL import Image
+from matplotlib import image
+import skimage.io
+import cv2
+
+#%% Image pre-process
+# Load the image
+img_test=cv2.imread('./Pikatchu_test.png', cv2.IMREAD_GRAYSCALE)
+# Process the data 
+img_process = preprocess_img(img_test)
+
+#%% Load the model
+filepath = 'model_denseNet201_pokemon.h5' # MAJ du modèle
+model = load_model(filepath)
+
+#%% Generate predictions for samples
+predictions = model.predict(img_process)
+print(predictions)
+
+# # Check if predictions contains 1 which corresponds to the mask
 one_value = np.argwhere(predictions>0)
 print(one_value)
