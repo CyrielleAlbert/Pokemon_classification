f196bc98fe3bc7bbb2e5c98b56c8677e29ca27a4
Sarra Margi <91186514+sarramargi@users.noreply.github.com>
2022-01-23 22:17:16 +0100

    Create main_pokemon.py

---
+++ main_pokemon.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 23 22:05:09 2022
+
+@author: sarramargi
+"""
+
+# Load librairies 
+
+# Load data 
+
+
+# Pre-process
+
+# Call model 
+
+# Kfold -> recréer un entraînement !!! - 
+
+# Choix du meilleur modele 
+
+# Tuning 
+
+# Run meilleur modele avec hyperparamètres fixés
+
+# 
+
+
+
+
